0ec29444a54fc951af351f2eb2f831d6ff103015
Santiago <santiagorodriguez17@seven4n.com>
2019-04-03 21:02:05 -0500

    fix: merge

--- packages/login-widget/src/components/index.jsx
+++ packages/login-widget/src/components/index.jsx
@@ -5,9 +5,10 @@ import { View, PrimaryButton } from '@copa/ui';
 import s from './styles.scss';
 
 const App = () => (
-  <div><PrimaryButton> here here from new scope </PrimaryButton>
-        <View className={s.view}>I am just j  </View>
-</div>
+  <div>
+    <PrimaryButton> here here from new scope </PrimaryButton>
+    <View className={s.view}>I am just j </View>
+  </div>
 );
 
 export default App;
